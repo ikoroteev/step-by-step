fe0da5f688242066709b6771a98ff264978581cf
ikoroteev <koroteev@pushwoosh.com>
2017-05-20 15:04:07 +0700

    add test

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"fmt"
 )
 
 func TestHandler(t *testing.T) {
@@ -29,6 +30,34 @@ func TestHandler(t *testing.T) {
 	if testingGreeting != expectedGreeting {
 		t.Fatalf("Wrong greeting '%s', expected '%s'", testingGreeting, expectedGreeting)
 	}
+}
+
+func TestHandlerFewCases(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	var testCases = []string{"/abc", "/f", "///","/As"}
+
+	for _, testCase := range testCases {
+		res, err := http.Get(ts.URL + testCase)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		greeting, err := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expectedGreeting := fmt.Sprintf("URL.Path = \"%s\"", testCase)
+		testingGreeting := strings.Trim(string(greeting), " \n")
+		if testingGreeting != expectedGreeting {
+			t.Fatalf("Wrong greeting '%s', expected '%s'", testingGreeting, expectedGreeting)
+		}
+	}
+
 }
 
 // TODO: add one more test together
